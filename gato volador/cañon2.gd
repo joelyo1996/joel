6520dfd89a5d72940b18e7965cf3a77b16eedc7b
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2019-05-02 15:03:17 -0300

    animacion_gato

---
+++ gato volador/cañon2.gd
@@ -0,0 +1,17 @@
+extends Node2D
+var bala = preload("res://bala.tscn")
+
+func _ready():
+	var bli = bala.instance()
+	add_child( bli)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	var bli = bala.instance()
+	add_child( bli)
+	
+	pass # Replace with function body.
