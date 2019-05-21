b27395bc9fa672eef519aa3aa1a6c487d1635b36
monchojoel <48567887+monchojoel@users.noreply.github.com>
2019-05-21 18:15:13 -0300

    barra de vida

--- gato volador/barra de vida/ProgressBar.gd
+++ gato volador/barra de vida/ProgressBar.gd
@@ -1,20 +1,6 @@
 extends ProgressBar
 
-var golpe = 50 
-var recuperacion = 30
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-func recuperar_vida():
-	value = max_value
-	
-func recuperar_vida_parcial():
-	value = value + recuperacion
-	if value>= max_value:
-		value = max_value
-
-
-	
+	pass # Replace with function body.	
