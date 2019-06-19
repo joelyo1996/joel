320e74ce8f879134e57cef013d830682c41205cf
monchojoel <48567887+monchojoel@users.noreply.github.com>
2019-06-19 03:24:26 -0300

    cambios v1.2

---
+++ gato volador/escenario2.gd
@@ -0,0 +1,35 @@
+extends Node2D
+export (PackedScene) var perro2
+export (PackedScene) var perro3
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$audiogato.play()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_timerperrorojo_timeout():
+	$caminorojo/PathFollow2D.set_offset(randi())
+	
+	var p2 = perro2.instance()
+	add_child(p2)
+	
+	p2.position = $caminorojo/PathFollow2D.position
+	pass # Replace with function body.
+
+
+func _on_timerperroverde_timeout():
+	$caminoperroverde/PathFollow2D.set_offset(randi())
+	
+	var p3 = perro3.instance()
+	add_child(p3)
+	
+	p3.position = $caminoperroverde/PathFollow2D.position
+	pass # Replace with function body.
