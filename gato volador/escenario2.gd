0a4547a727ce5ff1a9b1b98d1dc26781d98f61e3
monchojoel <48567887+monchojoel@users.noreply.github.com>
2019-06-20 11:34:48 -0300

    bajar nive

--- gato volador/escenario2.gd
+++ gato volador/escenario2.gd
@@ -12,7 +12,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (vida_global.puntos == 400):
+	if (vida_global.puntos == 100):
 		get_tree().change_scene("res://ganaste.tscn")
 	pass
 
