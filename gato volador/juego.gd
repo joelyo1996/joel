ac7c193687dc3e0f6e584b2a2fb37f618ad2d82c
monchojoel <48567887+monchojoel@users.noreply.github.com>
2019-06-23 20:11:20 -0300

    cambios de enemigo

--- gato volador/juego.gd
+++ gato volador/juego.gd
@@ -1,5 +1,6 @@
 extends Node2D
 export (PackedScene) var perro1
+export (PackedScene) var perro2
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -48,3 +49,13 @@ func _process(delta):
 	if (vida_global.puntos == 25):
 		get_tree().change_scene("res://pasaste_de_nivel.tscn")
 	
+
+
+func _on_perrorojotimer_timeout():
+	$camino2/PathFollow2D.set_offset(randi())
+	
+	var p2 = perro2.instance()
+	add_child(p2)
+	
+	p2.position = $camino2/PathFollow2D.position
+	pass # Replace with function body.
