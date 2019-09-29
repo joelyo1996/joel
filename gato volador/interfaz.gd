92404a7e62f816c2f97bbee3ceb8d0d6733df5a8
joel <joel1996bello@gmail.com>
2019-09-29 10:05:51 -0300

    salir

--- gato volador/interfaz.gd
+++ gato volador/interfaz.gd
@@ -18,7 +18,9 @@ func _on_jugar_pressed():
 
 
 func _on_salir_pressed():
+	OS.shell_open("https://joelyo1996.itch.io/")
 	get_tree().quit()
+	
 
 func _on_creditos_pressed():
 	get_tree().change_scene("res://creditos.tscn")
